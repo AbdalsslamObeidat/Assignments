152e2f60a67910049bc916bc2b0503908b8c1d23
AbdalsslamObeidat <abdalsslam.sketch@gmail.com>
2025-03-23 15:10:11 +0300

    Sort - Reverse - Two Pointers

--- Assignment19/script.js
+++ Assignment19/script.js
@@ -7,11 +7,42 @@
 
 // console.log(Arr);
 
-let arr = [
-    { name: "Hussam", age: 30 },
-    { name: "Ali", age: 25 },
-    { name: "Ahmad", age: 28 }
-];
 
-let Arr=arr.sort((a, b) => a.age - b.age);
-console.log(Arr);
+// let arr = [1, 5, 9, 6, 3, 87, 72, 23];
+
+// function rev(array) {
+//     for (let i = 0; i < array.length / 2; i++) {
+//         let temp = array[i];
+//         array[i] = array[array.length - 1 - i];
+//         array[array.length - 1 - i] = temp;
+//     }
+//     return array;
+// }
+
+// console.log(rev(arr)); 
+
+let arr = [1, 5, 9, 6, 3, 87, 72, 23];
+
+function find(array, target) {
+   array.sort((a,b)=>a-b);
+
+   let left=0;
+   let right=array.length-1;
+   while(left<right){
+    let sum=array[left]+array[right];
+    if(sum===target){
+        return [array[left],array[right]];
+    }
+    else if(sum<target){
+        left++;
+    }
+    else if(sum >target){
+        right--;
+    }
+   
+   }
+   return null;
+   
+}
+
+console.log(find(arr, 9)); 
